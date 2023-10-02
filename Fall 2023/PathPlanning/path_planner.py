4882d7781c92b93bf1483aceb2f40a56032409fb
ceaglebu <craig.eagleburger@gmail.com>
2023-10-02 18:22:49 -0400

    Better Structure

--- Fall 2023/PathPlanning/path_planner.py
+++ Fall 2023/PathPlanning/path_planner.py
@@ -9,7 +9,7 @@ class PathPlanner():
     def __init__(self, board:chess.Board) -> None:
         self.board = board
 
-    def plan_paths(self, move:chess.Move) -> list[Waypoint]:
+    def turn_paths(self, move:chess.Move) -> list[Waypoint]:
         '''
         Given move, generate waypoints in the order that they should be traveled
             - Waypoints contain target location and active piece
@@ -32,10 +32,10 @@ class PathPlanner():
 
         return waypoints 
     
-    def path(self, piece_type:chess.PieceType, start:chess.Square, target:chess.Square) -> list[tuple]:
+    def single_path(self, piece_type:chess.PieceType, start:chess.Square, target:chess.Square, capture=False) -> list[tuple]:
         '''
         Given starting/target locations and a piece type, generate list of points
-        piece needs to travel to
+        a single piece needs to travel to
         
         Path should generally reflect how a player would move the piece
            - ex. diagonal vs straight and then right for a bishop
@@ -45,5 +45,4 @@ class PathPlanner():
 
         # First make path assuming nothing is in the way
         # Check if path is obstructed
-        return [(chess.square_file(location) + .5, chess.square_rank(location) + .5)for location in [start, target]]
-    
+        return [(chess.square_file(location) + .5, chess.square_rank(location) + .5) for location in [start, target]]
