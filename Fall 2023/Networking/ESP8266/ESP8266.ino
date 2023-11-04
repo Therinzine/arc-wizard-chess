ab10803be3d02e3b48730871d85ee886c8be3df1
ArnavVWadhwa <arnav.v.wadhwa@gmail.com>
2023-11-04 16:26:23 -0400

    Switched to byte arrays

--- Fall 2023/Networking/ESP8266/ESP8266.ino
+++ Fall 2023/Networking/ESP8266/ESP8266.ino
@@ -1,9 +1,3 @@
-// To program the ESP8266:
-//    1) Plug ESP8266 into programmer
-//    2) Plug programmer into USB port
-//    3) Press the ESP8266's program button which is on the programmer
-//    4) Click on the Arduino IDE => button to program
-
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
@@ -11,49 +5,89 @@ const char* ssid = "wizardschess";
 const char* password = "wizardschess";
 unsigned int localPort = 12345;  // same as server
 const int STATUS_LED = 2;
-String deviceId = "ESP1";
+const byte deviceId = 1; // Device ID as a byte
+
+int startTime = millis();
 
 WiFiUDP udp;
 
-
 void setup() {
+    pinMode(STATUS_LED, OUTPUT);
     Serial.begin(115200);
     WiFi.begin(ssid, password);
     while (WiFi.status() != WL_CONNECTED) {
-        // digitalWrite(STATUS_LED, HIGH);
-        // delay(250);
-        // digitalWrite(STATUS_LED, LOW);
-        // delay(250);
+        digitalWrite(STATUS_LED, HIGH);
+        delay(250);
+        digitalWrite(STATUS_LED, LOW);
+        delay(250);
         Serial.println("Connecting to server");
-        delay(1000);
     }
 
     digitalWrite(STATUS_LED, HIGH);
 
     udp.begin(localPort);
+    sendInitPacket();
 
-    // Send a "hello" message with the unique device ID
-    udp.beginPacket("192.168.2.2", localPort);
-    udp.write(("INIT:" + deviceId).c_str());
-    udp.endPacket();
-    Serial.println("Sent init packet");
+
+
+    
 }
 
 void loop() {
-    int packetSize = udp.parsePacket();
-    if (packetSize) {
-        char incomingPacket[16];
-        int len = udp.read(incomingPacket, 16);
-        if (len > 0) {
-            incomingPacket[len] = 0; // Null-terminate
-        }
-        Serial.write(incomingPacket, len);  // Send to Raspberry Pi Pico
-    }
 
-    if (Serial.available()) {
-      String dataFromPico = Serial.readString();
-      udp.beginPacket(udp.remoteIP(), udp.remotePort());
-      udp.write(dataFromPico.c_str());
+
+  if (millis() - startTime > 1000) {
+    sendInitPacket();
+    startTime = millis();
+  }
+
+  int packetSize = udp.parsePacket();
+  if (packetSize) {
+      IPAddress remoteIP = udp.remoteIP(); // Store the sender's IP address
+      unsigned int remotePort = udp.remotePort(); // Store the sender's port number
+
+      byte incomingPacket[16] = {0}; // Initialize all elements to zero
+      int len = udp.read(incomingPacket, sizeof(incomingPacket));
+      if (len > 0) {
+          Serial.write(incomingPacket, len);  // Send to Raspberry Pi Pico
+          printByteArray(incomingPacket, len);
+          // Echo back to sender as acknowledgement
+          udp.beginPacket(remoteIP, remotePort);
+          udp.write(incomingPacket, len);
+          udp.endPacket();
+      }
+  }
+
+  // Check if the Pico has sent any data over Serial to forward
+  if (Serial.available()) {
+      byte outgoingPacket[16] = {0}; // Buffer to store data from Pico
+      size_t len = Serial.readBytes(outgoingPacket, sizeof(outgoingPacket));
+
+      // Echo back to sender
+      IPAddress remoteIP = udp.remoteIP();
+      unsigned int remotePort = udp.remotePort();
+      udp.beginPacket(remoteIP, remotePort);
+      udp.write(outgoingPacket, len);
       udp.endPacket();
   }
+}
+
+void sendInitPacket() {
+  // Send a "hello" message with the unique device ID
+    byte initPacket[] = {0xAA, deviceId, deviceId}; // Init packet
+    udp.beginPacket(IPAddress(255, 255, 255, 255), localPort); // Broadcasting
+    udp.write(initPacket, sizeof(initPacket));
+    udp.endPacket();
+    Serial.println("Broadcasted init packet");
+}
+
+void printByteArray(const byte* array, size_t length) {
+    for (size_t i = 0; i < length; i++) {
+        if (array[i] < 0x10) {
+            Serial.print("0"); // Print leading zero for single digit hex values
+        }
+        Serial.print(array[i], HEX); // Print byte in HEX
+        Serial.print(" "); // Add a space between bytes for readability
+    }
+    Serial.println(); // New line after printing the array
 }
--- Fall 2023/Networking/PythonTestServer.py
+++ Fall 2023/Networking/PythonTestServer.py
@@ -1,11 +1,18 @@
 import socket
 import threading
 
+# Usage
+# To initialize and use the server:
+# >>> from PythonTestServer import ESPServer
+# >>> server = ESPServer()
+# To send a command:
+# >>> server.send_command(1, bytearray([0x01, 0x02, ..., 0x10]))
+
 
 class ESPServer:
     def __init__(self, port=12345):
         self.UDP_PORT = port
-        self.BUFFER_SIZE = 1024
+        self.BUFFER_SIZE = 16  # Set the buffer size to 16 bytes for the byte array
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.sock.bind(("", self.UDP_PORT))
 
@@ -19,21 +26,32 @@ class ESPServer:
     def start_listening(self):
         while True:
             data, addr = self.sock.recvfrom(self.BUFFER_SIZE)
-            data_str = data.decode('utf-8')
-            print(f"Recieved packet data {data}, addr {addr}, str {data_str}")
-
-            # If the message starts with "INIT:", it's an initialization message from an ESP8266
-            if data_str.startswith("INIT:"):
-                device_id = data_str.split(":")[1]
-                self.devices[device_id] = addr[0]
-                print(f"Device {device_id} connected with IP {addr[0]}")
+            if len(data) <= 16:
+                # Handle the initialization sequence and normal commands
+                if data[0] == 0xaa:  # Check if this is an init packet
+                    device_id = data[1]
+                    checksum = data[2]
+                    # Perform a simple checksum validation
+                    if checksum == (device_id & 0xFF):
+                        self.devices[device_id] = addr[0]
+                        print(f"Device {device_id} initialized with IP {addr[0]}")
+                    else:
+                        print(f"Checksum mismatch for device ID {device_id}")
+                else:
+                    print(f"Received data from {addr[0]}: {data}")
             else:
-                # Handle other messages here
-                print(f"Received from {addr[0]}: {data_str}")
+                print(data)
+                print(f"Invalid packet size from {addr[0]}")
 
     def send_command(self, device_id, command):
         if device_id in self.devices:
             UDP_IP = self.devices[device_id]
-            self.sock.sendto(command.encode('utf-8'), (UDP_IP, self.UDP_PORT))
+            # Check if the command is a bytearray and its length is 16 or less
+            if isinstance(command, bytearray) and len(command) <= 16:
+                # Pad the command with zeros if it's less than 16 bytes
+                self.sock.sendto(command, (UDP_IP, self.UDP_PORT))
+            else:
+                print(f"Command must be a bytearray with a length of 16 or less.")
         else:
             print(f"No device found with ID {device_id}")
+
