557c8a3485381c12a82b59435206507fe72d7847
ceaglebu <craig.eagleburger@gmail.com>
2023-11-06 17:36:31 -0500

    Bugfixes for presentation

--- Fall 2023/PathPlanning/path_planner.py
+++ Fall 2023/PathPlanning/path_planner.py
@@ -14,8 +14,8 @@ class PathPlanner():
 
     def turn_paths(self, move:chess.Move) -> list[Path]:
         '''
-        Given move, generate waypoints in the order that they should be traveled
-            - Waypoints contain target location and active piece
+        Given move, generate Paths for in the order that they should be traveled
+            - Paths contain active piece code (ex. 'p1') and list of points
             - ex. Capturing piece moves next to captured piece, captured piece
                   moves away, capturing piece moves to captured piece's spot 
         
@@ -27,28 +27,40 @@ class PathPlanner():
         if not self.board.is_legal(move):
             return False
         
-        piece_type = self.board.piece_at(move.from_square).piece_type
         piece_id = self.board.piece_list[move.from_square]
 
         # Handle Capture
         if self.board.is_capture(move):
-            captured_piece_type = self.board.piece_at(move.to_square).piece_type
-            captured_piece_id = self.board.piece_list[move.to_square]
             
-            captured_position = chess.H8 + ((move.color == chess.BLACK) * 16) + len(self.board.capture_counts[not move.color])
+            captured_position = chess.H8 + ((self.board.turn == chess.BLACK) * 16) + self.board.capture_counts[not self.board.turn]
+            
+            if self.board.is_en_passant(move):
+                captured_piece_square = move.to_square + (8 * self.board.turn)
+                captured_piece_id = self.board.piece_list[captured_piece_square]
 
-            paths.append(Path(piece_id, self.single_path(piece_type, move.from_square, move.to_square, capture=True)))
-            paths.append(Path(captured_piece_id, self.single_path(captured_piece_type, move.to_square, captured_position, capture=True)))
-            paths.append(Path(piece_id, self.single_path(piece_type, move.to_square, move.to_square, capture=True)))
+                paths.append(Path(piece_id, self.single_path(move.from_square, move.to_square)))
+                paths.append(Path(captured_piece_id, self.single_path(captured_piece_square, captured_position, move_type="LEAVE")))
+                return paths
+            else:
+                captured_piece_id = self.board.piece_list[move.to_square]
+
+                paths.append(Path(piece_id, self.single_path(move.from_square, move.to_square, move_type="CAPTURE")))
+                paths.append(Path(captured_piece_id, self.single_path(move.to_square, captured_position, move_type="LEAVE")))
+                paths.append(Path(piece_id, self.single_path(move.to_square, move.to_square)))
+                return paths
 
         # Handle Castle
+        if self.board.is_castling(move):
+            if self.board.is_kingside_castling(move):
+                paths.append(Path(piece_id, self.single_path(move.from_square, move.to_square)))
+                return paths
         
         # Handle Standard moves
-        paths.append(Path(piece_id, self.single_path(piece_type, move.from_square, move.to_square)))
+        paths.append(Path(piece_id, self.single_path(move.from_square, move.to_square)))
 
         return paths
     
-    def single_path(self, piece_type:chess.PieceType, start:chess.Square, target:chess.Square, capture=False) -> list[tuple]:
+    def single_path(self, start:chess.Square, target:chess.Square, move_type="NORMAL") -> list[tuple]:
         '''
         Given starting/target locations and a piece type, generate list of points
         a single piece needs to travel to
@@ -57,20 +69,23 @@ class PathPlanner():
            - ex. diagonal vs straight and then right for a bishop
         
         Use self.board data to avoid other pieces on the board if necessary
+
+        move_type: "NORMAL", "CASTLE", "CAPTURE", or "LEAVE"
         '''
      
         startRank = chess.square_rank(start)
         startFile = chess.square_file(start)
         endRank = chess.square_rank(target)
         endFile = chess.square_file(target)
+        changeInRank = endRank - startRank
+        changeInFile = endFile - startFile
         moveInbetween = False
 
     #Knight path planning 1 path, not both paths
-        if piece_type == chess.KNIGHT:
+        if abs(changeInRank) == 2 and abs(changeInFile) == 1 or abs(changeInRank) == 1 and abs(changeInFile) == 2:
             moveInbetween = False 
             #determines how the ranks change
-            changeInRank = endRank - startRank
-            changeInFile = endFile - startFile
+            
             rankDirection = 1 if changeInRank > 0 else -1
             fileDirection = 1 if changeInFile > 0 else -1
             '''
@@ -147,7 +162,7 @@ class PathPlanner():
 
         #Capture Position, if this piece is capturing somethin. Will always move it back to same position (Top left, i think?). Does not move back to center of square. This done here or in other path?
         #OR caputred piece leaving board also go between squares
-        if (target > 63 or capture):
+        if (target > 63 or move_type=="CAPTURE"):
             return [(chess.square_file(location) + 1, chess.square_rank(location) + 1) for location in [start, target]]
         else:
             return [(chess.square_file(location) + .5, chess.square_rank(location) + .5) for location in [start, target]]
--- Fall 2023/PathPlanning/wizboard.py
+++ Fall 2023/PathPlanning/wizboard.py
@@ -5,25 +5,24 @@ class WizBoard(chess.Board):
     def __init__(self) -> None:
         super().__init__()
         self.path_planner = PathPlanner(self)
-        self.piece_list = ['r1', 'n1', 'b1', 'q', 'k', 'b2', 'n2', 'r2',
-                           'p1', 'p1', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8'] + \
+        self.piece_list = ['R1', 'N1', 'B1', 'Q', 'K', 'B2', 'N2', 'R2',
+                           'P1', 'P1', 'P3', 'P4', 'P5', 'P6', 'P7', 'P8'] + \
                           [''] * 32 + \
-                          ['P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7', 'P8',
-                           'R1', 'N1', 'B1', 'Q', 'K', 'B2', 'N2', 'R2'] + \
+                          ['p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8',
+                           'r1', 'n1', 'b1', 'q', 'k', 'b2', 'n2', 'r2'] + \
                           [''] * 32
+            # - this is to keep track of which specific knight/bishop/rook is where, chess module only tracks kind of piece.
+            # - this is important because there is more than one black knight robot for example
         
         self.capture_counts = [0, 0]
-                           
-        #                  "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
-        # - this is to keep track of which specific knight/bishop/rook is where, chess module only tracks kind of piece.
-        # - this is important because there is more than one black knight robot for example
+        
 
     def push(self, move: chess.Move) -> None:
         if self.is_capture(move):
-            captured_position = chess.H8 + ((move.color == chess.BLACK) * 16) + len(self.capture_counts[not move.color])
-            self.piece_list[captured_position] = self.piece_list(move.to_square)
+            captured_position = chess.H8 + 1 + ((self.turn == chess.BLACK) * 16) + self.capture_counts[not self.turn]
+            self.piece_list[captured_position] = self.piece_list[move.to_square]
 
-            self.capture_counts[not move.color] += 1
+            self.capture_counts[not self.turn] += 1
 
         # Handle Castle  (extra swaps)
         
