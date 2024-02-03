8401437d9e3c51b04be8370fea4060a4de2c5ac8
Emri0047 <96705082+Emri0047@users.noreply.github.com>
2024-02-03 16:22:41 -0500

    Messed with sending binary

---
+++ Fall 2023/PathPlanning/signal.py
@@ -0,0 +1,24 @@
+import chess
+from path_planner import *
+from wizboard import *
+
+class byte:
+    def waypoint_to_byte(waypoint):
+        #seperate the tuples from the waypoint
+        start = waypoint[0]
+        end = waypoint[1]
+        x1 = start[0]
+        y1 = start[1]
+        x2 = end[0]
+        y2 = end[1]
+        a = (y2 - y1)
+        b = (x2-x1)
+        #diagonal movement
+        if 
+
+        #Need to seperate the tuples, then from that determine what needs to happen (move forward, backward, turn, ect)
+            
+
+
+        
+
